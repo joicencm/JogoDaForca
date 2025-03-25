4a77bb21ef8d604f489ed97ba49026bd64ba94dc
joicencm <joicencm@gmail.com>
2025-03-25 19:28:15 -0300

    Estrutura Basica do Projeto

--- JogoDaForcaConsoleApp1/Program.cs
+++ JogoDaForcaConsoleApp1/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace JogoDaForcaConsoleApp1
-{
+﻿
+
     internal class Program
     {
+    //Versão 1 Estrutura Basica do Projeto
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("------------------------------------------------");
+                Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("------------------------------------------------");
+
+                Console.Write("Digite uma letra: ");
+                char chute = Console.ReadLine()[0]; // otém apenas um caractere do que o usuário digita
+
+                Console.WriteLine(chute);
+
+                Console.ReadLine();
+            }
         }
     }
-}
