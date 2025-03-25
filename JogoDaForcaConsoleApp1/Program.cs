c0ee19bd71b2d1301a007cd8525a706dbe7a85c5
joicencm <joicencm@gmail.com>
2025-03-25 19:19:02 -0300

    Add project files.

---
+++ JogoDaForcaConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JogoDaForcaConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
