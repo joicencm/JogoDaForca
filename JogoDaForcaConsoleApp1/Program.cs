9708212065c363dc585c7f048f906d15c7adc491
joicencm <joicencm@gmail.com>
2025-03-25 19:50:30 -0300

    Implementado exibição palavra chave

--- JogoDaForcaConsoleApp1/Program.cs
+++ JogoDaForcaConsoleApp1/Program.cs
@@ -1,23 +1,38 @@
 ﻿
 
-    internal class Program
+internal class Program
+{
+    //Versão 2: Exibir palavra oculta com traços
+    static void Main(string[] args)
     {
-    //Versão 1 Estrutura Basica do Projeto
-        static void Main(string[] args)
+        while (true)
         {
-            while (true)
+
+            string palavraSecreta = "MELANCIA";
+
+            char[] letrasEncontradas = new char[palavraSecreta.Length];
+
+            for (int caractere = 0; caractere < letrasEncontradas.Length; caractere++)
             {
-                Console.Clear();
-                Console.WriteLine("------------------------------------------------");
-                Console.WriteLine("Jogo da Forca");
-                Console.WriteLine("------------------------------------------------");
-
-                Console.Write("Digite uma letra: ");
-                char chute = Console.ReadLine()[0]; // otém apenas um caractere do que o usuário digita
-
-                Console.WriteLine(chute);
-
-                Console.ReadLine();
+                //acessar a array no indice 0
+                letrasEncontradas[caractere] = '_';
             }
+
+            string dicaDaPalavra = string.Join(" ",letrasEncontradas);
+
+            Console.Clear();
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("Jogo da Forca");
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("Palavra secreta: " + dicaDaPalavra);
+            Console.WriteLine("------------------------------------------------");
+
+            Console.Write("Digite uma letra: ");
+            char chute = Console.ReadLine()[0]; // otém apenas um caractere do que o usuário digita
+
+            Console.WriteLine(chute);
+
+            Console.ReadLine();
         }
     }
+}
